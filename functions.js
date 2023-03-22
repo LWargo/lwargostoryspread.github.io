937200c00a668beab98ddbcec1719b0b4d98ab86
LWargo <lwargo@trinity.edu>
2023-03-21 20:22:31 -0500

    add sarah eye button functionality

--- functions.js
+++ functions.js
@@ -28,3 +28,17 @@ function eye_vis(){
         y.style.visibility = "visible";
     }
 }
+function sarah_eye_vis(){
+    var x = document.getElementById("cc_left_sarah_eye");
+    if(x.style.visibility === "visible"){
+        x.style.visibility = "hidden";
+    } else {
+        x.style.visibility = "visible";
+    }
+    var y = document.getElementById("cc_right_sarah_eye");
+    if(y.style.visibility === "visible"){
+        y.style.visibility = "hidden";
+    } else {
+        y.style.visibility = "visible";
+    }
+}
