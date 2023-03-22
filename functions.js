6b16b5c4add5de92d1796be46640a83deb0aec1d
LWargo <lwargo@trinity.edu>
2023-03-21 19:40:44 -0500

    this should be everything I need for the bun hair

--- functions.js
+++ functions.js
@@ -9,3 +9,14 @@ function brown_hair_vis(){
         x.style.visibility = "visible";
     }
 }
+function bun_hair_vis(){
+    console.log("function began");
+    var x = document.getElementById("cc_bun_hair");
+    if(x.style.visibility === "visible"){
+        console.log("currently visible");
+        x.style.visibility = "hidden";
+    } else {
+        console.log("currently invisible");
+        x.style.visibility = "visible";
+    }
+}
