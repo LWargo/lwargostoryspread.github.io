9cd46d570cb6ab22594e75395eec80344aaf44b5
LWargo <lwargo@trinity.edu>
2023-03-21 19:26:13 -0500

    test with double quotes

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 function brown_hair_vis(){
-                
-    var x = document.getElementById('cc_brown_hair');
+    console.log("function began");
+    var x = document.getElementById("cc_brown_hair");
     if(x.style.visibility === "visible"){
         alert("currently visible");
         x.style.visibility = "hidden";
