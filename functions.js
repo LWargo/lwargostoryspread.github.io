c9128238cfb8b5e1abe0be31f8d589401eb2aa92
LWargo <lwargo@trinity.edu>
2023-03-21 20:10:44 -0500

    eye stuff

--- functions.js
+++ functions.js
@@ -15,11 +15,16 @@ function bun_hair_vis(){
     }
 }
 function eye_vis(){
-
-    var x = document.getElementById("cc_bun_hair");
+    var x = document.getElementById("cc_left_eye");
     if(x.style.visibility === "visible"){
         x.style.visibility = "hidden";
     } else {
         x.style.visibility = "visible";
+    }
+    var y = document.getElementById("cc_right_eye");
+    if(y.style.visibility === "visible"){
+        y.style.visibility = "hidden";
+    } else {
+        y.style.visibility = "visible";
     }
 }
