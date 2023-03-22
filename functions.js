232c977e779f9a463afff1d24d64849903ea65c1
LWargo <lwargo@trinity.edu>
2023-03-21 20:09:20 -0500

    moved right eyeball

--- functions.js
+++ functions.js
@@ -1,22 +1,25 @@
 function brown_hair_vis(){
-    console.log("function began");
     var x = document.getElementById("cc_brown_hair");
     if(x.style.visibility === "visible"){
-        console.log("currently visible");
         x.style.visibility = "hidden";
     } else {
-        console.log("currently invisible");
         x.style.visibility = "visible";
     }
 }
 function bun_hair_vis(){
-    console.log("function began");
     var x = document.getElementById("cc_bun_hair");
     if(x.style.visibility === "visible"){
-        console.log("currently visible");
         x.style.visibility = "hidden";
     } else {
-        console.log("currently invisible");
+        x.style.visibility = "visible";
+    }
+}
+function eye_vis(){
+
+    var x = document.getElementById("cc_bun_hair");
+    if(x.style.visibility === "visible"){
+        x.style.visibility = "hidden";
+    } else {
         x.style.visibility = "visible";
     }
 }
